a6d0aec5186b5542da99f492bbefc5c4c26ca32f
WEI <wei12f8158@gmail.com>
2021-12-19 14:29:28 +0800

    Added notes.

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 吳偉平 on 2021/10/7.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
